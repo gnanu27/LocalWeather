e19511e5f217baebadd2cdc859108c2bba0a91a2
gnanu27 <gnanavel.py2k@gmail.com>
2018-11-28 09:17:01 +0530

    Updates

---
+++ precache-manifest.41d8e273d019d4953ce70eb017edc698.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b25174ee945fa38b1cf6",
+    "url": "/LocalWeather/static/js/runtime~main.b25174ee.js"
+  },
+  {
+    "revision": "5a890da5e560d246baff",
+    "url": "/LocalWeather/static/js/main.5a890da5.chunk.js"
+  },
+  {
+    "revision": "916096b22f364af37683",
+    "url": "/LocalWeather/static/js/1.916096b2.chunk.js"
+  },
+  {
+    "revision": "5a890da5e560d246baff",
+    "url": "/LocalWeather/static/css/main.885b75ab.chunk.css"
+  },
+  {
+    "revision": "aa619908a5063674b2295a6a9b3158b1",
+    "url": "/LocalWeather/index.html"
+  }
+];
